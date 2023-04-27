076d459a7305abf8da2d872a843438a5540c7ed8
Nazar <Dumi_sj18@itstepru.onmicrosoft.com>
2023-04-27 15:25:26 +0500

    Изменил метод запроса на POST + различные изменения

--- Pages/AdminPage.cshtml.cs
+++ Pages/AdminPage.cshtml.cs
@@ -6,26 +6,6 @@ namespace Cinema.Pages
 {
     public class AdminPageModel : PageModel
     {
-        //[BindProperty]
-        //public string? ImdbId { get ; set; }
-
-        //[BindProperty]
-        //public int Hall { get; set; }
-
-        //[BindProperty]
-        //public int Year { get; set; }
-
-        //[BindProperty]
-        //public int Month { get; set; }
-
-        //[BindProperty]
-        //public int Day { get; set; }
-
-        //[BindProperty]
-        //public int Hour { get; set; }
-
-        //[BindProperty]
-        //public int Minute { get; set; }
         public FilmDataBase? Db { get; set; }
 
         public AdminPageModel()
@@ -33,53 +13,53 @@ namespace Cinema.Pages
             Db = new(new FilmDbJson());
         }
 
-        public async Task OnGet()
+        public async Task OnPost()
         {
-            if (!string.IsNullOrEmpty(Request.Query["id"]))
+            if (!string.IsNullOrEmpty(Request.Form["id"]))
             {
-                var id = Request.Query["id"];
-                var count = Request.Query["hall"].Count;
+                var id = Request.Form["id"];
+                var count = Request.Form["hall"].Count;
                 List<Session> sessions = new();
 
                 for (var i = 0; i < count; i++)
                 {
-                    var correctYear = !string.IsNullOrEmpty(Request.Query["year"][i]);
-                    var correctMonth = !string.IsNullOrEmpty(Request.Query["month"][i]);
-                    var correctDay = !string.IsNullOrEmpty(Request.Query["day"][i]);
-                    var correctMinute = !string.IsNullOrEmpty(Request.Query["minute"][i]);
+                    var correctYear = !string.IsNullOrEmpty(Request.Form["year"][i]);
+                    var correctMonth = !string.IsNullOrEmpty(Request.Form["month"][i]);
+                    var correctDay = !string.IsNullOrEmpty(Request.Form["day"][i]);
+                    var correctMinute = !string.IsNullOrEmpty(Request.Form["minute"][i]);
 
                     if (correctYear && correctMonth && correctDay && correctMinute)
                     {
                         sessions.Add(new Session(
-                            int.Parse(Request.Query["hall"][i]),
-                            int.Parse(Request.Query["year"][i]),
-                            int.Parse(Request.Query["month"][i]),
-                            int.Parse(Request.Query["day"][i]),
-                            int.Parse(Request.Query["hour"][i]),
-                            int.Parse(Request.Query["minute"][i])));
+                            int.Parse(Request.Form["hall"][i]),
+                            int.Parse(Request.Form["year"][i]),
+                            int.Parse(Request.Form["month"][i]),
+                            int.Parse(Request.Form["day"][i]),
+                            int.Parse(Request.Form["hour"][i]),
+                            int.Parse(Request.Form["minute"][i])));
                     }
                     else if (correctYear && correctMonth && correctDay && !correctMinute)
                     {
                         sessions.Add(new Session(
-                            int.Parse(Request.Query["hall"][i]),
-                            int.Parse(Request.Query["year"][i]),
-                            int.Parse(Request.Query["month"][i]),
-                            int.Parse(Request.Query["day"][i]),
-                            int.Parse(Request.Query["hour"][i])));
+                            int.Parse(Request.Form["hall"][i]),
+                            int.Parse(Request.Form["year"][i]),
+                            int.Parse(Request.Form["month"][i]),
+                            int.Parse(Request.Form["day"][i]),
+                            int.Parse(Request.Form["hour"][i])));
                     }
                     else if (!correctMinute && !correctYear)
                     {
                         sessions.Add(new Session(
-                        int.Parse(Request.Query["hall"][i]),
-                        int.Parse(Request.Query["hour"][i]))
+                        int.Parse(Request.Form["hall"][i]),
+                        int.Parse(Request.Form["hour"][i]))
                         );
                     }
                     else
                     {
                         sessions.Add(new Session(
-                            int.Parse(Request.Query["hall"][i]),
-                            int.Parse(Request.Query["hour"][i]),
-                            int.Parse(Request.Query["minute"][i]))
+                            int.Parse(Request.Form["hall"][i]),
+                            int.Parse(Request.Form["hour"][i]),
+                            int.Parse(Request.Form["minute"][i]))
                             );
                     }
                 }
